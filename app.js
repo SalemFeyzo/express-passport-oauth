d0b51f3bc5e9af3901cde8745a10ef53d9855a6e
SalemFeyzo <f1989salim@gmail.com>
2021-11-24 16:03:30 +0300

    add PORT env

--- app.js
+++ app.js
@@ -29,6 +29,8 @@ app.get("/", (req, res) => {
   res.render("home", { user: req.user })
 })
 
-app.listen(3000, () =>
+const port = process.env.PORT || 5000
+
+app.listen(port, () =>
   console.log("server is running on http://localhost:3000")
 )
